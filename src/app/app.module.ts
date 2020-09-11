49dbfca2b2b833d6addc07266f58ff1c485c8ae5
anibal-pagan <anibal.pagan@upr.edu>
2020-09-11 16:52:44 -0400

    MAde about page and started main page.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { AboutPageComponent } from './about-page/about-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent,
+    AboutPageComponent
   ],
   imports: [
     BrowserModule,
